dc9f4cd875aeeb3fb09fcda24676379bbfda2cfa
smaarmstrong <seanmaarmstrong@gmail.com>
2024-10-17 21:24:43 +0100

    add STATIC_ROOT to seetings.py and linting

--- backend/blog/migrations/0001_initial.py
+++ backend/blog/migrations/0001_initial.py
@@ -6,7 +6,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
     dependencies = [
@@ -15,14 +14,28 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='BlogPost',
+            name="BlogPost",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=255)),
-                ('content', models.TextField()),
-                ('date_published', models.DateField()),
-                ('category', models.CharField(max_length=100)),
-                ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("title", models.CharField(max_length=255)),
+                ("content", models.TextField()),
+                ("date_published", models.DateField()),
+                ("category", models.CharField(max_length=100)),
+                (
+                    "author",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
         ),
     ]
--- backend/blog/migrations/0002_alter_blogpost_category.py
+++ backend/blog/migrations/0002_alter_blogpost_category.py
@@ -4,15 +4,14 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('blog', '0001_initial'),
+        ("blog", "0001_initial"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='blogpost',
-            name='category',
+            model_name="blogpost",
+            name="category",
             field=models.CharField(blank=True, max_length=255, null=True),
         ),
     ]
