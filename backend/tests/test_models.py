2d970967e01b9158ba2cbf7dea0b747249c1a0f5
smaarmstrong <seanmaarmstrong@gmail.com>
2024-10-17 12:12:47 +0100

    lint

--- backend/tests/test_models.py
+++ backend/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from project.models import Project, Screenshot, TechStack
 
+
 class ProjectModelTest(TestCase):
     def setUp(self):
         self.project = Project.objects.create(
@@ -9,7 +10,7 @@ class ProjectModelTest(TestCase):
             price="Free",
             company="Lil Robo",
             tech_stack="Swift, Python",
-            website="https://www.cratenfc.com"
+            website="https://www.cratenfc.com",
         )
 
     def test_project_creation_with_optional_fields(self):
@@ -20,55 +21,49 @@ class ProjectModelTest(TestCase):
 
     def test_project_creation_without_optional_fields(self):
         project = Project.objects.create(
-            title="Simple Project",
-            description="A project without optional fields."
+            title="Simple Project", description="A project without optional fields."
         )
         self.assertEqual(project.title, "Simple Project")
         self.assertIsNone(project.company)
         self.assertIsNone(project.tech_stack)
         self.assertIsNone(project.website)
 
+
 class ScreenshotModelTest(TestCase):
     def setUp(self):
         self.project = Project.objects.create(
-            title="CrateNFC",
-            description="Create musical NFC cards."
+            title="CrateNFC", description="Create musical NFC cards."
         )
 
     def test_screenshot_creation(self):
         screenshot = Screenshot.objects.create(
-            project=self.project,
-            image="screenshots/test_screenshot.png"
+            project=self.project, image="screenshots/test_screenshot.png"
         )
         self.assertEqual(screenshot.project.title, "CrateNFC")
         self.assertEqual(screenshot.image, "screenshots/test_screenshot.png")
 
     def test_multiple_screenshots(self):
         screenshot1 = Screenshot.objects.create(
-            project=self.project,
-            image="screenshots/test_screenshot1.png"
+            project=self.project, image="screenshots/test_screenshot1.png"
         )
         screenshot2 = Screenshot.objects.create(
-            project=self.project,
-            image="screenshots/test_screenshot2.png"
+            project=self.project, image="screenshots/test_screenshot2.png"
         )
         screenshots = self.project.screenshot_set.all()
         self.assertEqual(screenshots.count(), 2)
         self.assertEqual(screenshots[0].image, "screenshots/test_screenshot1.png")
         self.assertEqual(screenshots[1].image, "screenshots/test_screenshot2.png")
 
+
 class TechStackModelTest(TestCase):
     def setUp(self):
         self.project = Project.objects.create(
-            title="CrateNFC",
-            description="Create musical NFC cards."
+            title="CrateNFC", description="Create musical NFC cards."
         )
 
     def test_tech_stack_creation(self):
         tech_stack = TechStack.objects.create(
-            project=self.project,
-            name="Swift",
-            logo="tech_stack/swift_logo.png"
+            project=self.project, name="Swift", logo="tech_stack/swift_logo.png"
         )
         self.assertEqual(tech_stack.project.title, "CrateNFC")
         self.assertEqual(tech_stack.name, "Swift")
@@ -76,14 +71,10 @@ class TechStackModelTest(TestCase):
 
     def test_multiple_tech_stack_entries(self):
         tech_stack1 = TechStack.objects.create(
-            project=self.project,
-            name="Swift",
-            logo="tech_stack/swift_logo.png"
+            project=self.project, name="Swift", logo="tech_stack/swift_logo.png"
         )
         tech_stack2 = TechStack.objects.create(
-            project=self.project,
-            name="Python",
-            logo="tech_stack/python_logo.png"
+            project=self.project, name="Python", logo="tech_stack/python_logo.png"
         )
         tech_stacks = self.project.techstack_set.all()
         self.assertEqual(tech_stacks.count(), 2)
