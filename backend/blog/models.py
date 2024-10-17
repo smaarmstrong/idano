e2b48d824ff2cc53a71939cc9c31cc79e2e936b7
smaarmstrong <seanmaarmstrong@gmail.com>
2024-10-17 19:32:51 +0100

    create a model for blog posts

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,1 +1,14 @@
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    date_published = models.DateField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
