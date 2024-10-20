771a37baf5ea5c829b734ac9f6003f94bc7d2870
smaarmstrong <seanmaarmstrong@gmail.com>
2024-10-20 21:04:51 +0100

    add serializers for blog and project

---
+++ backend/blog/serializer.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import BlogPost
+
+class BlogPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BlogPost
+        fields = '__all__'
+        extra_kwargs = {
+            'author': {'required': False}  # Make author not required
+        }
