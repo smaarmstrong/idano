d462fa6dffb8e1a9f3e55e204f579e16377e3a04
smaarmstrong <seanmaarmstrong@gmail.com>
2024-10-25 21:57:59 +0100

    create email message model

--- backend/emailmessage/models.py
+++ backend/emailmessage/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class EmailMessage(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    responded = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Email from {self.name} ({self.email}) - {self.subject}"
