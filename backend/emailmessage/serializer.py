8d4efe99db90020422a2653c0489d197e16373b4
smaarmstrong <seanmaarmstrong@gmail.com>
2024-10-25 22:24:09 +0100

    create serializer for emailmessage

---
+++ backend/emailmessage/serializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import EmailMessage
+
+class EmailMessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EmailMessage
+        fields = ['id', 'name', 'email', 'subject', 'message', 'created_at', 'responded']
+
