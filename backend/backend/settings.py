b62307e8eefc8d59cf01a828a22494e8d19bb0d6
smaarmstrong <seanmaarmstrong@gmail.com>
2024-11-13 10:33:01 +0000

    change postgres db settings in settings.py not to rely on a .env

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -87,13 +87,13 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 # Postgres
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("POSTGRES_DB"),
-        "USER": os.environ.get("POSTGRES_USER"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-        "HOST": "db",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
